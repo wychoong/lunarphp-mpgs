c7fceead17f144be54d66aca94fcaf35058e97c6
wychoong <wychoong@users.noreply.github.com>
2023-04-03 14:36:32 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'WyChoong\\MpgsPaymentType\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'WyChoong\\MpgsPaymentType\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
