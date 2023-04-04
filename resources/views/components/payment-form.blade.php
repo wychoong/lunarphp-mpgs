8311f78c21a6575ce122874f8cbfb5b485bf3ebe
wychoong <67364036+wychoong@users.noreply.github.com>
2023-04-04 17:00:09 +0800

    add js state for frontend

--- resources/views/components/payment-form.blade.php
+++ resources/views/components/payment-form.blade.php
@@ -1,10 +1,18 @@
 <div
     wire:key="mpgs-form"
     x-data="{
+        initializing: false,
+        processing: false,
         checkout(){
+            this.initializing = true
+
             $wire.checkout()
                 .then(resp => {
+                    this.initializing = false
+
                     if(resp.session){
+                        this.processing = true
+
                         Checkout.configure({
                             session:{
                                 id: resp.session
@@ -17,7 +25,7 @@
         }
     }"
     @checkout='checkout'
-    @mpgs-complete='$wire.checkoutSuccess()'
+    @mpgs-complete.window='$wire.checkoutSuccess()'
 >
     <x-lunar-mpgs::embed-container />
     <x-lunar-mpgs::checkout-button />
